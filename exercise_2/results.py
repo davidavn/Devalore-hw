f83252c6184e3172dc4d385aee8ac0fa0b07ee03
DavidAvnerson <42175069+DavidAvnerson@users.noreply.github.com>
2021-04-23 17:29:05 +0300

    resolve the second exercise

--- exercise_2/results.py
+++ exercise_2/results.py
@@ -1,15 +1,47 @@
 #!/usr/bin/python
 
+import os
+import re
+
+
+
+
+
+
+
+
+path = './main_folder'
+pictures = list()
+folders= list()
+d = dict()
 # 2. Write a python script to find all the pictures in all the folders.
 
+for root, directories, files in os.walk(path):
+    for filename in files:
+        if re.search('.+\\.jpg', filename):
+            pictures.append(filename)
+            parentDir = os.path.split(root)[1] 
+            d[parentDir] = filename
+    for dir in directories:
+        rootFullPath = os.path.join(os.getcwd(), root)
+        folders.append(os.path.join(rootFullPath, dir))
+
 # 3. Print the names of all the pictures.
+print('Name of the pictures:\n', "\n".join(pictures))
 
 # 4. Print the numbers of all the pictures.
+print('Number of pictures is ', len(pictures))
 
 # 5. Print the number of all the folders.
+print('Number of folders is ', len(folders))
 
 # 6. Print the full paths to all the folders.
+print('Full path of the folders:\n', '\n'.join(folders))
 
 # 7. Print dictionary with the key being the name of each picture's parent folder and value as the picture's name.
+print('Dictionary of pictures filename with parent folder:\n', d)
 
 # 8. Print a filtered list with the name of the folders that contains the letter 's'
+noPathFolders = [os.path.split(folderWithPath)[1] for folderWithPath in folders]
+Sfolders = [folder for folder in noPathFolders if re.search('.*s.*', folder)]
+print(Sfolders)
