6b7a18772fbb67fc4b8b7e75aa908d40aecaa0f9
DavidAvnerson <42175069+DavidAvnerson@users.noreply.github.com>
2021-04-23 15:21:54 +0300

    add folder with images

---
+++ exercise_2/results.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# 2. Write a python script to find all the pictures in all the folders.
+
+# 3. Print the names of all the pictures.
+
+# 4. Print the numbers of all the pictures.
+
+# 5. Print the number of all the folders.
+
+# 6. Print the full paths to all the folders.
+
+# 7. Print dictionary with the key being the name of each picture's parent folder and value as the picture's name.
+
+# 8. Print a filtered list with the name of the folders that contains the letter 's'
